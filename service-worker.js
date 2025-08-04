d1e582c3b61a3d8ca0588b165c661d1e8db17c0c
ahmedaidara <ahmedsaidaidara05@gmail.com>
2025-08-04 14:01:05 +0000

    Update and rename sw.js to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('ansar-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/main.css',
+        '/main.js',
+        '/assets/images/logo-192.png',
+        '/assets/images/logo-512.png',
+        '/assets/images/chatbot-bg.jpg',
+        '/offline.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== 'ansar-v1')
+                 .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => caches.match('/offline.html'));
+    })
+  );
+});
